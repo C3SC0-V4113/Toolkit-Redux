ba5bf77bf98d94fbbb7194329d628ce520f3d39e
CESCO <cescovalledev@gmail.com>
2023-07-02 21:50:41 -0600

    fetching without axios

--- src/slices/pokemon/thunks.js
+++ src/slices/pokemon/thunks.js
@@ -3,9 +3,15 @@ import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
+    const limit = 10;
 
-    // TODO : realizar peticion Http;}
+    // TODO : realizar peticion Http;
+    const resp = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page * limit}`
+    );
 
-    // dispatch(setPokemons());
+    const data = await resp.json();
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
