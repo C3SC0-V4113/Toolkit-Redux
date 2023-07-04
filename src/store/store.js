2791e59df39e880c4576c62eb066d625a4d4bd7a
CESCO <cescovalledev@gmail.com>
2023-07-03 21:52:30 -0600

    Todo App using RTK

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice, pokemonSlice } from "./slices";
+import { todosApi } from "./apis";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
